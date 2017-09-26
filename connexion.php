00923b3723352415c2570fecf2d2e84fe822819a
Delahaye Marien <delahaye.marien@gmail.com>
2017-09-26 09:29:53 +0200

    Edited the html part in index.php

---
+++ connexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<form action="inscription.php" method="post">
+    <input type="text" name="username" placeholder="Username"><br>
+    <input type="text" name="pass" placeholder="Password"><br>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Espace membres</title>
+    <title>User panel</title>
 </head>
 <body>
     <form action="inscription.php" method="post">
-    <input type="text" name="pseudo" placeholder="pseudo"><br>
-    <input type="text" name="email" placeholder="email"><br>
-    <input type="password" name="pass" placeholder="Mot de passe"><br>
-    <input type="password" name="pass" placeholder="Retapez votre mot de passe"><br>
-    <input type="submit" value="Valider">
+    <input type="text" name="pseudo" placeholder="Username"><br>
+    <input type="text" name="email" placeholder="Email address"><br>
+    <input type="password" name="pass" placeholder="Password"><br>
+    <input type="password" name="pass" placeholder="Retype your password"><br>
+    <input type="submit" value="Register">
 
     </form>
 
